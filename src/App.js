153833e52a3589746b458f10d56adb364f894af4
davydka <davydka@gmail.com>
2020-06-09 11:34:59 -0700

    chore: initial commit, setup theme global and reset styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classnames from 'classnames/bind';
+
+import styles from './App.module.scss';
+
+const cx = classnames.bind(styles);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={cx('app')}>
+      hello world
     </div>
   );
 }
