b7cb8b70fe265ec552297fa13be8f25c32852c3d
davydka <davydka@gmail.com>
2020-06-29 12:06:17 -0700

    feat: WIP exploring scripting blender

--- pocs/framework-comparisons/src_models/exploring.py
+++ pocs/framework-comparisons/src_models/exploring.py
@@ -9,25 +9,26 @@ print ("The script has the name %s" % (sys.argv[0]))
 print ("Arguments of the script %s" % argv)
 
 
+def convertToText(obj):
+    bpy.context.view_layer.objects.active = obj
+    bpy.context.active_object.select_set(state=True)
+    bpy.ops.object.convert(target="MESH")
+
+
 
 ## Count the arguments
-#arguments = len(sys.argv) - 1
+arguments = len(argv) - 1
 
 ## Output argument-wise
-#position = 1
-#while (arguments >= position):
-#    print ("Parameter %i: %s" % (position, sys.argv[position]))
-#    position = position + 1
+position = 0
+while (arguments >= position):
+    target = "Text"+str(position + 1).zfill(2)
+    print ("%s: %s" % (target, argv[position]))
+    # bpy.data.objects[target].data.body = argv[position]
+    # convertToText(bpy.data.objects[target])
+    position = position + 1
 
 # bpy.data.objects["Text01"].data.body = "Alice"
 
-
-#def convertToText(obj):
-#    bpy.context.view_layer.objects.active = obj
-#    bpy.context.active_object.select_set(state=True)
-#    bpy.ops.object.convert(target="MESH")
-    
 #convertToText(bpy.data.objects["Text01"])
-#convertToText(bpy.data.objects["Text02"])
-
 
